b0d3f766e9353194b15803b22e891d685a0a15c8
Ryo Ota <nwtgck@nwtgck.org>
2023-02-22 20:16:32 +0900

    create --server-command option

--- go.mod
+++ go.mod
@@ -5,10 +5,13 @@ go 1.19
 require (
 	github.com/fatih/color v1.14.1
 	github.com/google/uuid v1.3.0
+	github.com/spf13/cobra v1.6.1
 )
 
 require (
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/sys v0.3.0 // indirect
 )
--- main/main.go
+++ main/main.go
@@ -5,40 +5,56 @@ import (
 	"fmt"
 	"github.com/fatih/color"
 	"github.com/nwtgck/piping-server-check/check"
+	"github.com/spf13/cobra"
+	"os"
 )
 
-func run() error {
-	checks := check.AllChecks()
-	config := check.Config{
-		// TODO:
-		RunServerCmd: []string{"sh", "-c", "piping-server --http-port=$HTTP_PORT"},
-	}
-	for _, c := range checks {
-		subConfig := check.SubConfig{
-			// TODO:
-			Protocol: check.Http1_1,
-		}
-		result := check.RunCheck(&c, &config, &subConfig)
-		jsonBytes, err := json.Marshal(&result)
-		if err != nil {
-			return err
-		}
+var flag struct {
+	serverCommand string
+}
 
-		line := string(jsonBytes)
-		if len(result.Errors) == 0 {
-			line = color.GreenString(fmt.Sprintf("✔︎%s", line))
-		} else {
-			line = color.RedString(fmt.Sprintf("✖︎%s", line))
-			//line = color.YellowString(fmt.Sprintf("⚠︎%s", line))
+func init() {
+	cobra.OnInitialize()
+	rootCmd.PersistentFlags().StringVarP(&flag.serverCommand, "server-command", "", "", "Command to run a Piping Server")
+	rootCmd.MarkPersistentFlagRequired("server-command")
+}
+
+var rootCmd = &cobra.Command{
+	Use:   os.Args[0],
+	Short: "Check Piping Server",
+	RunE: func(_ *cobra.Command, args []string) error {
+		checks := check.AllChecks()
+		config := check.Config{
+			// TODO: sh -c
+			RunServerCmd: []string{"sh", "-c", flag.serverCommand},
 		}
-		fmt.Println(line)
-	}
-	return nil
+		for _, c := range checks {
+			subConfig := check.SubConfig{
+				// TODO:
+				Protocol: check.Http1_1,
+			}
+			result := check.RunCheck(&c, &config, &subConfig)
+			jsonBytes, err := json.Marshal(&result)
+			if err != nil {
+				return err
+			}
+
+			line := string(jsonBytes)
+			if len(result.Errors) == 0 {
+				line = color.GreenString(fmt.Sprintf("✔︎%s", line))
+			} else {
+				line = color.RedString(fmt.Sprintf("✖︎%s", line))
+				//line = color.YellowString(fmt.Sprintf("⚠︎%s", line))
+			}
+			fmt.Println(line)
+		}
+		return nil
+	},
 }
 
 func main() {
-	err := run()
-	if err != nil {
-		panic(err)
+	if err := rootCmd.Execute(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(-1)
 	}
 }
