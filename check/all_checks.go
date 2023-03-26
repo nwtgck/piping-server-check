08378cae03f1ee000323330bb2e897c2efb71102
Ryo Ota <nwtgck@nwtgck.org>
2023-03-26 21:40:25 +0900

    move "simultaneous_request" check

--- check/all_checks.go
+++ check/all_checks.go
@@ -5,14 +5,14 @@ func AllChecks() []Check {
 		post_first(),
 		get_first(),
 		put(),
-		simultaneous_request(),
 		post_cancel_post(),
 		get_cancel_get(),
 		service_worker_registration_rejection(),
 		post_first_byte_by_byte_streaming(),
 		multipart_form_data(),
 
-		// long check
+		// long checks
+		simultaneous_request(),
 		post_first_chunked_long_transfer(),
 	}
 }
